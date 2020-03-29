3872ad0d97344f3497a0e0b238b1b8c19c30245f
Paul downing <pauldowning@pauls-mbp.lan>
2020-03-29 16:48:08 -0500

    eject and add jest testing setup

--- test/app.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import App from '/app';
-
-describe('App', () => {
-  it('renders app', () => {
-    let wrapper = shallow(<App/>)
-    console.log(wrapper)
-  });
-});
