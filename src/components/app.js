4ffc4401c48a46f481be1beaf6ca0d00d6356028
Paul downing <pauldowning@pauls-mbp.lan>
2020-03-30 00:00:09 -0500

    mostly about section with some logo, footer, and general styling updates

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 import '../styles/app.css';
 import { Button } from 'react-bootstrap';
 import Navbar from './navbar';
+import ProfilePhoto from './profile-photo';
+import About from './about';
+import Footer from './footer';
 
 function App() {
   const dash = '—'
@@ -9,10 +12,12 @@ function App() {
     <div className="App">
       <div className="banner-image">
         <Navbar />
-
         <h1 className="nameplate">Paul Downing</h1>
         <p className="subplate-text">{dash}{dash} Software Engineer {dash}{dash}</p>
+        <ProfilePhoto />
       </div>
+      <About />
+      <Footer />
     </div>
   );
 }
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Logo from '../../images/logo-transparent.png';
 import NavbarLink from './navbar-link';
 
 export default function Navbar() {
   return (
-    <nav className="navbar fixed-top navbar-expand-lg">
+    <nav className="navbar navbar-expand-lg">
       <div className="container">
-        <a className="navbar-brand" href="#">Insert Logo Here</a>
+        <a className="navbar-brand" href="#">
+          <img className="logo" src={Logo}/>
+        </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <NavbarLink href="#" linkText="About"/>
