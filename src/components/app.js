c15883f833f5ff9a5157b57b5baa31d5acc59873
Paul downing <pauldowning@pauls-mbp.lan>
2020-03-29 21:50:00 -0500

    add nameplate over banner image

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,14 @@ import { Button } from 'react-bootstrap';
 import Navbar from './navbar';
 
 function App() {
+  const dash = '—'
   return (
     <div className="App">
       <div className="banner-image">
         <Navbar />
+
+        <h1 className="nameplate">Paul Downing</h1>
+        <p className="subplate-text">{dash}{dash} Software Engineer {dash}{dash}</p>
       </div>
     </div>
   );
