beaba478a6a24ab681812e4dc7ace68f22f4aa16
Paul Downing <downing034@gmail.com>
2022-11-08 21:46:55 -0600

    update resume with current job stuff

---
+++ src/components/resume/employment_history.js
@@ -0,0 +1,66 @@
+export const EMPLOYMENT_HISTORY = [
+  {
+    companyName: "Kava Labs",
+    city: "(Remote)",
+    state: "Denver, CO",
+    jobTitle: "Senior Software Engineer II",
+    startYear: "2020",
+    endYear: "Present"
+  },
+  {
+    companyName: "TeamSnap",
+    city: "(Remote)",
+    state: "Boulder, CO",
+    jobTitle: "Software Engineer",
+    startYear: "2019",
+    endYear: "2020"
+  },
+  {
+    companyName: "Granicus",
+    city: "Denver",
+    state: "CO",
+    jobTitle: "Software Engineer",
+    startYear: "2017",
+    endYear: "2019"
+  },
+  {
+    companyName: "Granicus",
+    city: "Denver",
+    state: "CO",
+    jobTitle: "Quality Assurance Engineer",
+    startYear: "2015",
+    endYear: "2017"
+  },
+  {
+    companyName: "SPS Commerce",
+    city: "Minneapolis",
+    state: "MN",
+    jobTitle: "Quality Assurance Analyst",
+    startYear: "2014",
+    endYear: "2015"
+  },
+  {
+    companyName: "SPS Commerce",
+    city: "Minneapolis",
+    state: "MN",
+    jobTitle: "Support Center Lead",
+    startYear: "2013",
+    endYear: "2014"
+  },
+  {
+    companyName: "SPS Commerce",
+    city: "Minneapolis",
+    state: "MN",
+    jobTitle: "Senior Customer Operations Analyst",
+    startYear: "2013",
+    endYear: "2013"
+  },
+  {
+    companyName: "SPS Commerce",
+    city: "Minneapolis",
+    state: "MN",
+    jobTitle: "Customer Operations Analyst",
+    startYear: "2012",
+    endYear: "2013"
+  },
+]
