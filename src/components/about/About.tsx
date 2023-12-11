a9536923ef5df92ab0ead344021145c3471f10ca
Paul D <downing034@gmail.com>
2023-12-11 02:15:43 -0700

    adjust styling for responsiveness

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -9,11 +9,11 @@ const About = () => {
       <div className="about-container white-container">
         <div className="container">
           <div className="row">
-            <div className="col-sm-7">
+            <div className="col-sm-12 col-lg-7 col-xl-7">
               <img className="potato" src={Potato} alt="dog" />
             </div>
-            <div className="col-sm-5">
-              <h1 className="section-header">A few fun facts about myself</h1>
+            <div className="col-sm-12 col-lg-5 col-xl-5">
+              <h1 className="section-header">About Me</h1>
               <p className="section-description">I work out of Denver, CO, I have a dog named Potato, my favorite number is 34, sour beers are my jam, I play Rocket League like it's going out of style, I enjoy running, metal is my top Spotify genre, I make/sell candles, I'm an avid cross-stitcher.</p>
             </div>
           </div>
--- src/components/portfolio/PortfolioItem.tsx
+++ src/components/portfolio/PortfolioItem.tsx
@@ -13,24 +13,25 @@ const PortfolioItem = ({ projectTitle }: PortfolioItemProps) => {
   const { image, altText, title, description, githubUrl, siteUrl, comingSoon } = useProject(projectTitle);
   return (
     <div className="box">
-      <h4 className="portfolio-title">{title}</h4>
-      <img className="portfolio-icon" src={image} alt={altText}/>
+      <div className="box-background">
+        <h4 className="portfolio-title">{title}</h4>
+        <img className="portfolio-icon" src={image} alt={altText}/>
 
-      <div className="middle1">
-        <div>{description}</div>
-        <br />
-        <a id="github" className="text" target="_blank" href={githubUrl}>VIEW CODE</a>
-        
+        <div className="middle1">
+          <div>{description}</div>
+          <br />
+          <a id="github" className="text" target="_blank" href={githubUrl}>VIEW CODE</a>
+          
+        </div>
+
+        <div className="middle2">
+
+          {comingSoon && <div className="soon-text">Coming soon...</div>}
+
+          {!comingSoon && <a id="site" className="text" target="_blank" href={siteUrl}>VIEW SITE</a>}
+          
+        </div>
       </div>
-
-      <div className="middle2">
-
-        {comingSoon && <div className="soon-text">Coming soon...</div>}
-
-        {!comingSoon && <a id="site" className="text" target="_blank" href={siteUrl}>VIEW SITE</a>}
-        
-      </div>
-
       <PortfolioItemDetails projectTitle={title} />
     </div>
   )
