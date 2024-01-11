e545bdf35487c0156405b21423dd9fe0de0ee18f
Paul Downing <downing034@gmail.com>
2024-01-10 21:00:41 -0600

    Update abilities.ts
    re-order abilities

--- src/constants/abilities.ts
+++ src/constants/abilities.ts
@@ -1,57 +1,66 @@
 import { Abilities } from './types';
 
 export const CODING_ABILITIES: Abilities = [
-  { name: 'AWS / Google Cloud', skillLevel: 1 },
-  { name: 'CI / CD', skillLevel: 3 },
-  { name: 'Create React App', skillLevel: 5 },
-  { name: 'CSS/3 (+ SASS)', skillLevel: 4 },
-  { name: 'Docker', skillLevel: 3 },
-  { name: 'GIT', skillLevel: 4 },
-  { name: 'Github / Gitlab', skillLevel: 4 },
-  { name: 'Heroku', skillLevel: 3 },
+  { name: 'Create React App / Vite', skillLevel: 5 },
   { name: 'HTML/5', skillLevel: 5 },
   { name: 'Javascript', skillLevel: 5 },
   { name: 'JSON / XML', skillLevel: 5 },
-  { name: 'Kubernetes', skillLevel: 3 },
-  { name: 'NATS Streaming Server', skillLevel: 2 },
+  { name: 'React', skillLevel: 5 },
+  { name: 'Redux', skillLevel: 5 },
+  { name: 'Typescript', skillLevel: 5 },
+
+  { name: 'CSS/3 (+ SASS)', skillLevel: 4 },
+  { name: 'GIT', skillLevel: 4 },
+  { name: 'Github / Gitlab', skillLevel: 4 },
   { name: 'Netlify', skillLevel: 4 },
+  { name: 'Ruby', skillLevel: 4 }, 
+  { name: 'Webpack', skillLevel: 4},
+  
+  { name: 'CI / CD', skillLevel: 3 },
+  { name: 'Docker', skillLevel: 3 },
+  { name: 'Heroku', skillLevel: 3 },
+  { name: 'Kubernetes', skillLevel: 3 },
   { name: 'NextJS', skillLevel: 3 },
   { name: 'NodeJS / Express', skillLevel: 3 },
   { name: 'Rails', skillLevel: 3 },
-  { name: 'React', skillLevel: 5 },
-  { name: 'Redux', skillLevel: 5 },
-  { name: 'Ruby', skillLevel: 4 },
-  { name: 'Typescript', skillLevel: 5 },
-  { name: 'Vite', skillLevel: 3 },
-  { name: 'Webpack', skillLevel: 4},   
+
+  { name: 'NATS Streaming Server', skillLevel: 2 },
+
+  { name: 'AWS / Google Cloud', skillLevel: 1 },
 ];
 
 export const DESIGN_ABILITIES: Abilities = [
+  { name: 'Material UI', skillLevel: 5 },
   
   { name: 'Bootstrap', skillLevel: 4 },
+  
   { name: 'Framer Motion', skillLevel: 2 },
-  { name: 'Material UI', skillLevel: 5 },
+  
   { name: 'Tailwind', skillLevel: 2 },
 ];
 
 export const TESTING_ABILITIES: Abilities = [
-  { name: 'Capybara', skillLevel: 3 },
-  { name: 'Cucumber', skillLevel: 3 },
-  { name: 'Cypress', skillLevel: 2 },
   { name: 'Enzyme', skillLevel: 5 },
   { name: 'Jest', skillLevel: 5 },
-  { name: 'PlayWright', skillLevel: 2 },
   { name: 'React Testing Library', skillLevel: 5 },
+  
+  { name: 'Capybara', skillLevel: 3 },
+  { name: 'Cucumber', skillLevel: 3 },
   { name: 'Rspec', skillLevel: 3 },
+  
+  { name: 'Cypress', skillLevel: 2 },
+  { name: 'PlayWright', skillLevel: 2 },
 ];
 
 export const TOOLS_ABILITIES: Abilities = [
   { name: 'Apple / MacOS', skillLevel: 5 },
-  { name: 'Figma', skillLevel: 2 },
-  { name: 'Jira / Atlassian', skillLevel: 4 },
   { name: 'Object Oriented Programming', skillLevel: 5 },
-  { name: 'Shortcut', skillLevel: 4 },
   { name: 'Scrum / Agile', skillLevel: 5 },
+
+  { name: 'Jira / Atlassian', skillLevel: 4 },
+  { name: 'Shortcut', skillLevel: 4 },
+  
+  { name: 'Figma', skillLevel: 2 },
 ];
 
 export const LANGUAGE_ABILITIES: Abilities = [
